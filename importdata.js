0b1dd904f8ef4290c0adc1a29339e27f99220c70
bekkamvaishnavi <vaishnavy941@gmail.com>
2016-02-21 18:15:05 -0800

    data

---
+++ importdata.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('vkApp')
+  .controller('importdata', function(
+      $scope,
+      $log
+      ) {
+
+      $scope.metaform = {
+        importdata: ''
+      };
+
+
+
+
+
+
+    });
